7de67e47962b7f9347d58d8182bd784a9a451747
pi3rrot <4dakto@gmail.com>
2019-08-13 22:04:52 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "My Blog",
+    description: "This is my cool blog.",
+  },
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`,
+      },
+    },
+  ],
 }
